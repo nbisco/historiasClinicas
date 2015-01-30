ef0ffbae9ba1eac90fd4cb4520721e567a9d3971
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2015-01-30 06:47:14 -0300

    Agregue la coleccion Pacntes y la UI para agregarlos

--- historiasClinicas.js
+++ historiasClinicas.js
@@ -1,20 +1,46 @@
+Pacientes = new Mongo.Collection('pacientes');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.body.helpers({
+    agregarHistoria: function() {
+      return Session.get('agregarHistoria');
+    },
+    contarHistorias: function() {
+      return Pacientes.find().count();
+    },
+    paciente: function() {
+      return Pacientes.find({}, {sort: {creadoEl: -1}});
+    },
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.body.events({
+    "click #nuevaHistoria": function () {
+      Session.set('agregarHistoria', true);
+    },
+    "submit #formHistoria": function (e) {
+      var paciente = {
+        apellido: e.target.apellido.value,
+        nombre: e.target.nombre.value,
+        edad: e.target.edad.value,
+        telefono: e.target.telefono.value,
+        dni: e.target.dni.value,
+        domicilio: e.target.domicilio.value,
+        obraSocial: e.target.obraSocial.value 
+      };
+      Meteor.call('insertarPaciente', paciente);
+      Session.set('agregarHistoria', false);
+      return false;
+    },
   });
+  
 }
+
+Meteor.methods({
+  insertarPaciente: function (paciente) {
+    paciente.creadoEl = new Date();
+    Pacientes.insert(paciente);
+  },
+});
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
